15cffa38f8d54190405dd9c012fe02112e5c5e00
Gampesh <gampesh@gmail.com>
2022-01-27 13:45:53 +0530

    new leetcode

--- palindrome-number.js
+++ palindrome-number.js
@@ -1,4 +1,6 @@
 /*
+https://leetcode.com/problems/palindrome-number
+
 Given an integer x, return true if x is palindrome integer.
 
 An integer is a palindrome when it reads the same backward as forward.
